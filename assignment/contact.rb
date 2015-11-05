ba394f357b3f1e3f6cc504d443a3201d20d4ef9a
Hershel Nashman <hershel.nashman@gmail.com>
2015-11-05 14:22:59 -0500

    find contact runs

--- assignment/contact.rb
+++ assignment/contact.rb
@@ -1,32 +1,93 @@
 class Contact
-	attr_reader :id
-	attr_accessor :first_name, :last_name, :email, :notes
+  attr_accessor :first_name, :last_name, :email, :note, :id
+  @@contacts = []
+  @@id = 1000
 
-	@@contacts = []
-	@@id = 1
+  def initialize(first_name, last_name, email, note)
+    @first_name = first_name
+    @last_name = last_name
+    @email = email
+    @note = note
 
-def initialize(first_name, last_name, email, notes)
-	@first_name = first_name
-	@last_name = last_name
-	@email = email
-	@notes = notes
-	@id = @@id
-	@@id += 1
-end
+    @id = @@id
+    @@id += 1
+  end
 
-def self.create(first_name, last_name, email, notes )
-	new_contact = Contact.new(first_name, last_name, email, notes)
+  def self.delete_all
+    @@contacts = []
+    @@id = 1
+  end
 
-	@@contacts << new_contact
+  def delete
+    @@contacts.delete(self)
+  end
 
-end
+  def self.all
+    @@contacts
+  end
 
-def self.all
-	@@contacts
-end
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 
-def full_name
-	"#{first_name} #{last_name}"
-end
+  def self.create(first_name, last_name, email, note)
+    new_contact = Contact.new(first_name, last_name, email, note)
+    @@contacts << new_contact
+    new_contact
+  end
 
+  def update(field, new_value)
+    if field == "note"
+      self.note = new_value
+    elsif field == "first_name"
+      self.first_name = new_value
+    elsif field == "last_name"
+      self.last_name = new_value
+    elsif field == "email"
+      self.email = new_value
+    end
+  end
+
+  def self.search_by_attribute(search, result)
+    array = []
+    if search == "first_name"
+      @@contacts.each do |find|
+        if find.first_name == result
+          array << find
+        end
+      end
+    elsif search == "last_name"
+      @@contacts.each do |find|
+        if find.last_name == result
+          array << find
+        end
+      end
+    elsif search == "email"
+      @@contacts.each do |find|
+        if find.email == result
+          array << find
+        end
+      end
+    elsif search == "note"
+      @@contacts.each do |find|
+        if find.note == result
+          array << find
+        end
+      end
+    end
+    array
+  end
+
+
+  def self.get(id)
+    @@contacts.each do |contact|
+      if contact.id == id
+        return contact
+      end
+    end
+  end
+
+  def self.find(contact_id)
+    @@contacts.find { |contact| contact.id == contact_id }
+  end
 end
--- assignment/crm.rb
+++ assignment/crm.rb
@@ -1,9 +1,11 @@
 require_relative 'contact'
 require 'sinatra'
 
+Contact.create("Johnny", "Bravo", "johnny@bitmakerlabs.com", "Rockstar")
+
 
 get '/' do
-  @crm_app_name = "My CRM"
+  @crm_app_name = "my CRM"
   erb :index
 end
 
@@ -13,6 +15,11 @@ end
 
 get '/contacts/new' do
   erb :new_contact
+end
+
+get "/contacts/1000" do
+  @contact = Contact.find(1000)
+  erb :show_contact
 end
 
 post '/contacts' do
