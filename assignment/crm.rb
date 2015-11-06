e873de7fd34afbac3a5a5c1a4303779424fc63aa
Hershel Nashman <hershel.nashman@gmail.com>
2015-11-06 14:24:37 -0500

    all working, no database

--- assignment/crm.rb
+++ assignment/crm.rb
@@ -1,5 +1,22 @@
 require_relative 'contact'
 require 'sinatra'
+#require 'data_mapper'
+
+#DataMapper.setup(:default, "sqlite3:database.sqlite3")
+
+#class Contact
+
+	#include DataMapper::Resource
+#  attr_accessor :id, :first_name, :last_name, :email, :note
+
+#  def initialize(first_name, last_name, email, note)
+#    @first_name = first_name
+#    @last_name = last_name
+#    @email = email
+#    @note = note
+#  end
+#end
+
 
 get '/' do
   @crm_app_name = "my CRM"
